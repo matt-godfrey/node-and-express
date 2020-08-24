e7d492bdab50924b6a23a404cc7f55e4a63a158f
Matt Godfrey <godfrey.m1992@gmail.com>
2020-08-24 18:43:17 -0400

    mounted body-parser middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 require('dotenv').config()
 // --> 7)  Mount the Logger middleware here
 app.use((req, res, next) => {
@@ -9,7 +10,7 @@ app.use((req, res, next) => {
 })
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 /** 1) Meet the node console. */
 
