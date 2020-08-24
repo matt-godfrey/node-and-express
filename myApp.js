eb9bdb67c8e77835b62bbdf94e2184d2318a77e3
Matt Godfrey <godfrey.m1992@gmail.com>
2020-08-24 18:13:29 -0400

    Get query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -60,7 +60,11 @@ app.get("/:word/echo", (req, res) => {
 })
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get("/name", (req, res) => {
+    var first = req.query.first;
+    var last = req.query.last;
+    res.json({ name: first + " " + last })
+})
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
